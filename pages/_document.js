394166d44fc83b6825c1825ed32974ef95ed1cd9
ssukhawani <ssukhawani@gmail.com>
2021-11-27 16:29:32 +0530

    removed meta tag

--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,6 @@ class AppDocument extends Document {
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
                 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap" rel="stylesheet"></link>
                 <meta name="description" content="FrontendFreck helps you to make your dev journey more exciting with cool resources"/>
-                <meta name="a.validate.01" content="25266fe4a74005982391266db7d330a1d544" />
                 </Head>
                 <body>
                     <Main></Main>
