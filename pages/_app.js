79d8d88f8ef2bcb7925df239517f0d9780416baf
Sahil Sukhawani <ssukhawani@gmail.com>
2022-08-09 20:31:37 +0530

    onsignal push

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,14 @@ import Script from "next/script";
 
 import "../styles/globals.scss";
 import "react-responsive-modal/styles.css";
+import OneSignal from "react-onesignal";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    OneSignal.init({ appId: "0cf37607-f7c7-459c-8258-6cd271c6a25b" });
+  });
+
   return (
     <>
       <Script
